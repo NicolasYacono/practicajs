11f8374ce791467727b06549aa734ac78dd20e1f
Nicolas Yacono <nmyacono@gmail.com>
2022-07-02 13:24:34 -0300

    Main to Practica y Practica 2

--- practica2.js
+++ practica2.js
@@ -1,8 +1,8 @@
 //PRACTICA
 alert("Bienvenida/o! Le informamos que necesita ser mayor de edad y poseer reprocann para poder inscribirse")
 alert("Ambos seran corroborados con el numero de DNI")
-let dni = Number(prompt("Ingrese su DNI"))
-while(dni = ""){
+let dni = Number(prompt("Ingrese su DNI (sin puntos)"))
+while(dni === "."){
     console.log("Ingrese su DNI (sin puntos)")
 };
 if(dni >= 45000000){
@@ -10,7 +10,7 @@ if(dni >= 45000000){
 } else {
     let nombre = prompt("Ingrese su nombre");
     console.log("Hola "+nombre+" podra acceder a:");
-    const abonoMensual = (gramos, precio) => gramos * precio;
+    const abonoMensual = (gramos) => gramos;
     let membresia = Number(prompt("Ingrese el numero del tipo de membresia que desea \n1 - Premium \n2 - Clasica\n3 - Gratuita"));
         if(membresia === 3 ){
             console.log("Contenido de la web y primer asesoramiento gratis")
@@ -18,30 +18,30 @@ if(dni >= 45000000){
             console.log("Beneficios exclusivos, hasta 15gr mensuales, asesoramiento gratis por un mes y contenido de la web")
             const gramos1 = Number(prompt("Ingrese la cantidad de gramos que desea 5,10 o 15"));
             let precio1;
-            const abonoMensualGramos = abonoMensual (gramos1, precio1);
+            const abonoMensualGramos = abonoMensual (gramos1);
             if(gramos1 === 5){
-                let precio1 = 900;
-                console.log("Abono mensual por 5gr $"+abonoMensualGramos+"")
+                precio1 = abonoMensualGramos * 900;
+                console.log("Abono mensual por 5gr $"+precio1+"")
             } else if (gramos1 === 10) {
-                let precio1 = 850;
+                precio1 = 850;
                 console.log("Abono mensual por 10gr $"+abonoMensualGramos+"")
             } else {
-                let precio1 = 800;
+                precio1 = 800;
                 console.log("Abono mensual por 15gr $"+abonoMensualGramos+"")
             }
         } else if (membresia === 1){
             console.log("Asesoramiento y cursos gratiutos, hasta 30gr mensuales, beneficios premium, contenido de la web")
             const gramos1 = Number(prompt("Ingrese la cantidad de gramos que desea 10,20 o 30"));
-            let precio1 = 900;
-            const abonoMensualGramos = abonoMensual (gramos1, precio1);
+            precio1 = 900;
+            const abonoMensualGramos = abonoMensual (gramos1);
             if(gramos1 === 10){
-                let precio1 = 850;
+                precio1 = 850;
                 console.log("Abono mensual por 10gr $"+abonoMensualGramos+"")
             } else if (gramos1 === 20) {
-                let precio1 = 800;
+                precio1 = 800;
                 console.log("Abono mensual por 20gr $"+abonoMensualGramos+"")
             } else {
-                let precio1 = 750;
+                precio1 = 750;
                 console.log("Abono mensual por 30gr $"+abonoMensualGramos+"")
             }
         }else{
